8492fd8861e680a833d8d5203405c744a4f8e68a
sotomaque <sotomaque@gmail.com>
2020-01-30 13:40:44 -0800

    Updates

---
+++ precache-manifest.e62b5688e9053b167ddd7e3701d4f0e1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc271f4f979f2862a79809674f1198a8",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "95c06797fbe77cefd5ed",
+    "url": "/portfolio/static/css/main.3180bdec.chunk.css"
+  },
+  {
+    "revision": "43bd8b92fda0f09d94af",
+    "url": "/portfolio/static/js/2.eceb827e.chunk.js"
+  },
+  {
+    "revision": "95c06797fbe77cefd5ed",
+    "url": "/portfolio/static/js/main.91790839.chunk.js"
+  },
+  {
+    "revision": "3593b2f94172585f4863",
+    "url": "/portfolio/static/js/runtime-main.3b58c541.js"
+  }
+]);
